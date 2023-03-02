90d5e46131d4ab59056e26177e7bddec04f3dda1
Vira Murray <vira.avramenko@gmail.com>
2023-03-02 15:05:52 -0500

    Creating DOM elements S 11 L 147

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +90,37 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = ['a', 'b', 'c', 'd', 'e'];
+//slice (does not mutate the original array)
+console.log(arr.slice(2));
+// (3) ['c', 'd', 'e']
+console.log(arr.slice(2, 4));
+//(2) ['c', 'd']
+console.log(arr.slice(-2));
+//(2) ['d', 'e']
+console.log(arr.slice(-1));
+//['e']
+console.log(arr.slice(1, -2));
+//(2) ['b', 'c']
+console.log(arr.slice());
+//(5) ['a', 'b', 'c', 'd', 'e']
+console.log([...arr]);
+//(5) ['a', 'b', 'c', 'd', 'e']
+//================================================
+//splice (mutates the original array)
+// console.log(arr.splice(2));
+//(3) ['c', 'd', 'e']
+arr.splice(-1);
+console.log(arr);
+//(2) ['a', 'b']
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+//reverse (mutates the original array)
+console.log(arr2.reverse());
+
+//concat (does not mutate the original array)
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//join
+console.log(letters.join(' - '));
